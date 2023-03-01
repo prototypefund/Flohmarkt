e9fc488657d103680c64b10e04bd253952bc5c84
grindhold <grindhold@gmx.net>
2023-03-01 22:45:09 +0100

    working flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,69 @@
 {
-  description = "Flohmarkt development flake";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  outputs = { self, nixpkgs } : rec
+  {
+    system = "x86_64-linux";
+    packages.x86_64-linux.default = 
+      with import nixpkgs {inherit system;};
+      python3Packages.buildPythonPackage {
+        pname = "flohmarkt";
+        name = "flohmarkt";
+        src = ./.;
+        nativeBuildInputs = [
+          python3
+          python3Packages.fastapi
+          python3Packages.uvicorn
+        ];
+        propagatedBuildInputs = [
+          python3
+          python3Packages.fastapi
+          python3Packages.uvicorn
+        ];
+      };
+    devShell.x86_64-linux =
+      with import nixpkgs {inherit system;};
+      mkShell {
+        name = "flohmarkt devshell";
+        buildInputs = [
+          python3
+          python3Packages.fastapi
+          python3Packages.uvicorn
+        ];
+    };
+    nixosModule.default = { config, lib, ... }:
+      {
+        options = {
 
-  outputs = { self, nixpkgs }: {
-    defaultPackage.x86_64-linux = 
-    with import <nixpkgs> { system = "x86_64-linux" }
-    buildPythonpackage rec {
-      pname = "flohmarkt";
-      src = ./flohmarkt;
-    }
-  }
+          services.devflake = {
+
+            enable = lib.mkOption {
+              default = false;
+              type = lib.types.bool;
+              description = ''
+                Enable agent service. Use this to disable but still install the service at packer time.
+              '';
+            };
+
+          };
+        };
+
+        config = lib.mkIf config.services.devflake.enable {
+          systemd.services.devflake = {
+            description = "flek runner";
+            wantedBy = [ "multi-user.target" ];
+            after = [ "network.target" ];
+            script = ''
+              cd ${./.}
+              ${nixpkgs.legacyPackages.x86_64-linux.python3Packages.uvicorn}/bin/uvicorn devflake:app
+            '';
+          };
+
+          environment.systemPackages = [ packages.x86_64-linux.default ] ++ [
+                nixpkgs.legacyPackages.x86_64-linux.python3
+                nixpkgs.legacyPackages.x86_64-linux.python3Packages.fastapi
+                nixpkgs.legacyPackages.x86_64-linux.python3Packages.uvicorn
+          ];
+        };
+      };
+  };
 }
