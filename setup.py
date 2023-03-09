de97fd9ad46eaec9f11ef988af38899beb91bfd8
grindhold <grindhold@gmx.net>
2023-03-09 14:11:51 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='flohmarkt',
+    version='0.2.0',
+    description='a decentral federated small ad platform',
+    author='grindhold',
+    author_email='grindhold+floh@skarphed.org',
+    url='http://codeberg.org/grindhold/flohmarkt',
+    packages=['flohmarkt'],
+)
