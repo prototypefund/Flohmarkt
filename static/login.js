597844613b33928c78d7eae8a8b6fc94b17de980
grindhold <grindhold@gmx.net>
2023-03-11 16:52:47 +0100

    wipstate of button click

---
+++ static/login.js
@@ -0,0 +1,8 @@
+
+function getToken() {
+    console.log("LOL");
+}
+
+console.log(document.querySelector('.login_button'));
+document.querySelector('.login_button').addEventListener("onclick", getToken);
+console.log("bar");
