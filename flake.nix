f7faf6ee453432d6789990565fe27ad23af8e85c
grindhold <grindhold@gmx.net>
2023-03-12 04:29:33 +0100

    provide flohmarkt conf througj nix module

--- flake.nix
+++ flake.nix
@@ -44,6 +44,122 @@
                 Enable flohmarkt service.
               '';
             };
+            settings = {
+              general = {
+                instanceName = lib.mkOption {
+                  default = "Fluffys Flomarkt";
+                  type = lib.types.str;
+                  description = ''
+                    The name the Instance will be presented with
+                    to the users
+                  '';
+                };
+                externalUrl = lib.mkOption {
+                  default = "https://example.org";
+                  type = lib.types.str;
+                  description = ''
+                    The external URL under which this instance will
+                    be available
+                  '';
+                };
+                debugMode = lib.mkOption {
+                  default = 0;
+                  type = lib.types.int;
+                  description = ''
+                    If this is 1, debug mode will be active.
+                    WARNING: Running in Debug may compromise
+                    your operational security.
+                  '';
+                };
+                jwtSecret = lib.mkOption {
+                  default = "S0op3rs3cr3t";
+                  type = lib.types.str;
+                  description = ''
+                    This is the secret that will be used for HS256
+                    JWT generation and validation.
+                  '';
+                };
+              };
+              database = {
+                server = lib.mkOption {
+                  default = "127.0.0.1";
+                  type = lib.types.str;
+                  description = ''
+                    The IP of the MongoDB Server we are going to connect
+                    to for persistent storage
+                  '';
+                };
+                port = lib.mkOption {
+                  default = 27017;
+                  type = lib.types.int;
+                  description = ''
+                    The Port MongoDB Server we are going to connect
+                    to for persistent storage
+                  '';
+                };
+                user = lib.mkOption {
+                  default = "user";
+                  type = lib.types.str;
+                  description = ''
+                    The Username used to authenticate with MongoDB
+                  '';
+                };
+                password = lib.mkOption {
+                  default = "S0op3rs3cr3t";
+                  type = lib.types.str;
+                  description = ''
+                    The password used to authenticate with MongoDB
+                  '';
+                };
+              };
+              smtp = {
+                server = lib.mkOption {
+                  default = "S0op3rs3cr3t";
+                  type = lib.types.str;
+                  description = ''
+                    The SMTP Server used for outgoing mail
+                  '';
+                };
+                port = lib.mkOption {
+                  default = 25;
+                  type = lib.types.int;
+                  description = ''
+                    The Port we're goint to use on the SMTP Server
+                  '';
+                };
+                user = lib.mkOption {
+                  default = "user@example.org";
+                  type = lib.types.str;
+                  description = ''
+                    The username with which we connect to the SMTP Server
+                    (usually looks like an email address)
+                  '';
+                };
+                from = lib.mkOption {
+                  default = "otheruser@example.org";
+                  type = lib.types.str;
+                  description = ''
+                    The email adress which is displayed as From-address
+                    in outgoing email.
+                  '';
+                };
+                password = lib.mkOption {
+                  default = "S0op3rs3cr3t";
+                  type = lib.types.str;
+                  description = ''
+                    The password used to authenticate with the Mailserver
+                  '';
+                };
+                cafile = lib.mkOption {
+                  default = "/some/path";
+                  type = lib.types.str;
+                  description = ''
+                    If the SMTP-Server has a self-signed cert, place it
+                    somewhere and put the path here.
+                  '';
+                };
+              };
+            };
           };
         };
 
@@ -52,24 +168,24 @@
           environment.etc."flohmarkt.conf" = {
             text = ''
               [General]
-              InstanceName = Fluffys Flohmarkt
-              ExternalURL = http://fluffy.cat
-              DebugMode = 0
-              JwtSecret = S0op3rs3cr3t
+              InstanceName = ${config.services.flohmarkt.settings.general.instanceName}
+              ExternalURL = ${config.services.flohmarkt.settings.database.externalUrl}
+              DebugMode = ${config.services.flohmarkt.settings.database.debugMode}
+              JwtSecret =  ${config.services.flohmarkt.settings.database.jwtSecret}
 
               [Database]
-              Server = 192.168.0.52
-              Port = 27017
-              User = foo
-              Password = bar
+              Server = ${config.services.flohmarkt.settings.database.server}
+              Port = ${config.services.flohmarkt.settings.database.port}
+              User = ${config.services.flohmarkt.settings.database.user}
+              Password = ${config.services.flohmarkt.settings.database.password}
 
               [SMTP]
-              Server = mail.foo.org
-              Port = 587
-              User = user@foobar.org
-              From = user@example.org
-              Password = S0op3rs3cr3t
-              CAFile = foo.pem
+              Server = ${config.services.flohmarkt.settings.smtp.server}
+              Port = ${config.services.flohmarkt.settings.smtp.port}
+              User = ${config.services.flohmarkt.settings.smtp.user}
+              From = ${config.services.flohmarkt.settings.smtp.from}
+              Password = ${config.services.flohmarkt.settings.smtp.password}
+              CAFile = ${config.services.flohmarkt.settings.smtp.cafile}
             '';
           };
           systemd.services.flohmarkt = {
