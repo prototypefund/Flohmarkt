448e74e81e93cb14bac4258b4f46e2f553813acf
grindhold <grindhold@gmx.net>
2023-03-15 13:21:55 +0100

    correctly 401 when using get_current_user

--- flohmarkt/auth.py
+++ flohmarkt/auth.py
@@ -1,6 +1,6 @@
 import jwt
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 
 from flohmarkt.config import cfg
@@ -9,5 +9,9 @@ from flohmarkt.models.user import UserSchema
 oauth2 = OAuth2PasswordBearer(tokenUrl="token")
 
 async def get_current_user(token : str = Depends(oauth2)):
-    dec = jwt.decode(token, cfg["General"]["JwtSecret"], algorithms=["HS512"])
-    return await UserSchema.retrieve_single_id(dec["user_id"])
+    try:
+        dec = jwt.decode(token, cfg["General"]["JwtSecret"], algorithms=["HS512"])
+    except Exception as e:
+        raise HTTPException(status_code=401, detail=str(e))
+    else:
+        return await UserSchema.retrieve_single_id(dec["user_id"])
