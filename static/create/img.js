7c5df5b847a584c146120186273d897ee4f8b91e
midzer <midzer@gmail.com>
2023-03-15 15:52:24 +0100

    introduce cats

--- static/create/img.js
+++ static/create/img.js
@@ -2,7 +2,7 @@ import { createElement } from "./element.js";
 
 export function createImg(src, alt, className=null) {
     const image = createElement('img', className);
-    image.src = '/static/' + src;
+    image.src = src.endsWith('.svg') ? '/static/' + src : src;
     image.alt = alt;
 
     return image;
--- static/create/item.js
+++ static/create/item.js
@@ -4,9 +4,11 @@ import { createElement } from "./element.js";
 
 export function createItem(item) {
     const element = createElement('aside', 'position-relative');
-    element.appendChild(createImage(item.image.src, item.image.alt));
-    element.appendChild(createLink('/~' + item.user + '/' + item.id, 'd-block stretched-link', item.name));
-    element.appendChild(createElement('p', null, item.price + item.currency));
+    element.appendChild(createImage(item.image.src, item.image.alt, 'card-img-top w-100'));
+    const wrapper = createElement('div', 'p-2');
+    wrapper.appendChild(createLink('/~' + item.user + '/' + item.id, 'stretched-link', item.name));
+    wrapper.appendChild(createElement('p', null, item.price + item.currency));
+    element.appendChild(wrapper);
 
     return element;
 }
