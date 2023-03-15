65e926a8e8dc32683cea928a93212bd47e2fcf11
midzer <midzer@gmail.com>
2023-03-15 17:11:54 +0100

    improve item css

--- static/create/item.js
+++ static/create/item.js
@@ -6,8 +6,10 @@ export function createItem(item) {
     const element = createElement('aside', 'position-relative');
     element.appendChild(createImage(item.image.src, item.image.alt, 'card-img-top w-100'));
     const wrapper = createElement('div', 'p-2');
-    wrapper.appendChild(createLink('/~' + item.user + '/' + item.id, 'stretched-link', item.name));
-    wrapper.appendChild(createElement('p', null, item.price + item.currency));
+    const container = createElement('div', 'd-flex justify-content-between');
+    container.appendChild(createLink('/~' + item.user + '/' + item.id, 'stretched-link', item.name));
+    container.appendChild(createElement('span', null, item.price + item.currency));
+    wrapper.appendChild(container);
     wrapper.appendChild(createElement('p', null, item.description));
     element.appendChild(wrapper);
 
