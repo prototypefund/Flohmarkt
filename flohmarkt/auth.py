1c8e6b32a7ccecd0f784f99f2c6b19457cc606bd
grindhold <grindhold@gmx.net>
2023-03-15 13:18:07 +0100

    implemented get_current_user

--- flohmarkt/auth.py
+++ flohmarkt/auth.py
@@ -4,9 +4,10 @@ from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 
 from flohmarkt.config import cfg
+from flohmarkt.models.user import UserSchema
 
 oauth2 = OAuth2PasswordBearer(tokenUrl="token")
 
 async def get_current_user(token : str = Depends(oauth2)):
-    x = jwt.decode(token, cfg["General"]["JwtSecret"], algorithms=["HS512"])
-    print(x)
+    dec = jwt.decode(token, cfg["General"]["JwtSecret"], algorithms=["HS512"])
+    return await UserSchema.retrieve_single_id(dec["user_id"])
