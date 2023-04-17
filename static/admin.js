b29f8f9c532481e3c7004a60ddf6d5a7172ae53d
grindhold <grindhold@gmx.net>
2023-04-17 16:33:16 +0200

    preliminary form for instance settings

--- static/admin.js
+++ static/admin.js
@@ -37,7 +37,57 @@ users.forEach(user => {
     usersFragment.appendChild(controlsContainer);
 });
 
+const instanceFragment = document.createDocumentFragment();
+
+const instanceHeading = createElement('h2', null, 'Site Settings');
+instanceFragment.appendChild(instanceHeading);
+const instanceForm = createElement('form', null, '');
+const instanceRulesTextArea = createElement('textarea');
+instanceRulesTextArea.id = "instance_rules";
+const instanceAboutTextArea = createElement('textarea');
+instanceRulesTextArea.id = "instance_about";
+const instanceRulesLabel = createElement('label', null, 'Rules');
+const instanceAboutLabel = createElement('label', null, 'About');
+instanceRulesLabel.for = "instance_rules";
+instanceAboutLabel.for = "instance_about";
+const instanceSaveButton = createElement('button', null, 'Save');
+instanceSaveButton.addEventListener('click', event => {
+    event.preventDefault();
+
+    const formData = new FormData(instanceForm);
+    postJSON("/api/v1/admin/", {
+        coordinates: formData.get('instance_coordinates'),
+        perimeter: formData.get('instance_perimeter'),
+        rules: formData.get('instance_rules'),
+        about: formData.get('instance_about'),
+    })
+    .then(async data => {
+	// reload
+    });
+});
+const instanceCoordinatesInput = createElement('input');
+instanceCoordinatesInput.id = "instance_coordinates";
+const instanceRangeInput = createElement('input');
+instanceRangeInput.id = "instance_perimeter";
+const instanceCoordinatesLabel = createElement('label', null, 'Location of your Site (lat:lon e.g. 51.213:23.24)');
+const instanceRangeLabel = createElement('label', null, 'Range for your Site in km'
+);
+instanceCoordinatesLabel.for = "instance_coordinates";
+instanceRangeLabel.for = "instance_perimeter";
+
+instanceForm.appendChild(instanceCoordinatesLabel);
+instanceForm.appendChild(instanceCoordinatesInput);
+instanceForm.appendChild(instanceRangeLabel);
+instanceForm.appendChild(instanceRangeInput);
+instanceForm.appendChild(instanceRulesLabel);
+instanceForm.appendChild(instanceRulesTextArea);
+instanceForm.appendChild(instanceAboutLabel);
+instanceForm.appendChild(instanceAboutTextArea);
+instanceForm.appendChild(instanceSaveButton);
+instanceFragment.appendChild(instanceForm);
+
 const gridAdmin = document.querySelector('.grid__admin');
 window.requestAnimationFrame(() => {
     gridAdmin.appendChild(usersFragment);
+    gridAdmin.appendChild(instanceFragment);
 });
