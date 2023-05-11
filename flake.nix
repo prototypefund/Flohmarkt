c6891fbf16f496ff9e5928cf13c02e1e38a014b3
grindhold <grindhold@gmx.net>
2023-05-11 11:28:57 +0200

    background-service: autostart

--- flake.nix
+++ flake.nix
@@ -223,6 +223,7 @@
               User = "flohmarkt";
             };
             after = [ "network.target" ];
+            wantedBy = [ "multi-user.target" ];
             script = ''
               cd ${./.}
               ${nixpkgs.legacyPackages.x86_64-linux.python3.withPackages (p: depfun p ++ [packages.x86_64-linux.default ])}/bin/python3 background.py
