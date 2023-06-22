b22ded3cb46dd71834db7bb7fd3f5b4bf5f2c83a
grindhold <grindhold@gmx.net>
2023-06-22 17:58:50 +0200

    function to generate no-avatar circle

--- static/create/avatar.js
+++ static/create/avatar.js
@@ -1,8 +1,19 @@
 import { createImage } from "./image.js";
+import { createElement } from "./element.js";
+
+function createNoAvatarCircle(small) {
+    const av = createElement('div', '', '');
+    av.style.height=small?"32px":"48px";		
+    av.style.width=small?"32px":"48px";	
+    av.classList = [small?"small_avatar":"avatar"];	
+    av.style.display="inline-block";		
+    av.style.backgroundColor="crimson";
+    return av;
+}
 
 export function createAvatar(user) {
     if (user.avatar == null) {
-        return createImage("/static/img/user.svg", user.name, 'avatar circle');
+        return createNoAvatarCircle(false);
     } else if (user.avatar.startsWith("http")) {
         return createImage(user.avatar, user.name, 'avatar circle');
     } else {
@@ -11,7 +22,7 @@ export function createAvatar(user) {
 }
 export function createSmallAvatar(user) {
     if (user.avatar == null) {
-        return createImage("/static/img/user.svg", user.name, 'small_avatar circle');
+        return createNoAvatarCircle(true);
     } else if (user.avatar.startsWith("http")) {
         return createImage(user.avatar, user.name, 'small_avatar circle');
     } else {
