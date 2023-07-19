375861e68085f503079fd0fbef0f9c3ba9f523b0
grindhold <grindhold@gmx.net>
2023-07-19 20:10:28 +0200

    show default pic on items

--- static/create/item.js
+++ static/create/item.js
@@ -11,40 +11,43 @@ export function createItem(item, details=false, watching=[]) {
         element = createElement('aside', 'position-relative card-item');
     }
     if (item.images && item.images.length > 0) {
-	const image = createImage("/api/v1/image/"+item.images[0] /*item.image.src*/, item.name/*item.image.alt*/, 'card-img-top w-100');
-	image["image_idx"] = 0;
-	if (details) {
-	    const g_left = createElement("div", "galery_left", "");
-	    g_left.appendChild(createElement("span", null, "<"));
-	    g_left.addEventListener('click', e => {
-	        image.image_idx--;
-		if (image.image_idx < 0) { image.image_idx = item.images.length-1; }
-	        image.src = "/api/v1/image/"+item.images[image.image_idx%item.images.length];
-	        return true;
-	    });
-	    element.appendChild(g_left);
-	    const g_right = createElement("div", "galery_right", "")
-	    g_right.appendChild(createElement("span",null,">"));
-	    g_right.addEventListener('click', e => {
-	        image.image_idx++;
-	        image.src = "/api/v1/image/"+item.images[image.image_idx%item.images.length];
-	        return true;
-	    });
-	    element.appendChild(g_right);
-	} else {
-	    element.addEventListener('mouseenter', e => {
-            var callback = null;
-            const f = ()=>{
-                image.image_idx++;
-                image.src = "/api/v1/image/"+item.images[(image.image_idx+1)%item.images.length];
-                callback = window.setTimeout(f, 1000);
-            };
-            callback = window.setTimeout(f, 1000);
-            element.addEventListener('mouseout', e=> {
-                window.clearTimeout(callback);   
+        const image = createImage("/api/v1/image/"+item.images[0], item.name, 'card-img-top w-100');
+        image["image_idx"] = 0;
+        if (details) {
+            const g_left = createElement("div", "galery_left", "");
+            g_left.appendChild(createElement("span", null, "<"));
+            g_left.addEventListener('click', e => {
+                image.image_idx--;
+            if (image.image_idx < 0) { image.image_idx = item.images.length-1; }
+                image.src = "/api/v1/image/"+item.images[image.image_idx%item.images.length];
+                return true;
             });
-	    });
-	}
+            element.appendChild(g_left);
+            const g_right = createElement("div", "galery_right", "")
+            g_right.appendChild(createElement("span",null,">"));
+            g_right.addEventListener('click', e => {
+                image.image_idx++;
+                image.src = "/api/v1/image/"+item.images[image.image_idx%item.images.length];
+                return true;
+            });
+            element.appendChild(g_right);
+        } else {
+            element.addEventListener('mouseenter', e => {
+                var callback = null;
+                const f = ()=>{
+                    image.image_idx++;
+                    image.src = "/api/v1/image/"+item.images[(image.image_idx+1)%item.images.length];
+                    callback = window.setTimeout(f, 1000);
+                };
+                callback = window.setTimeout(f, 1000);
+                element.addEventListener('mouseout', e=> {
+                    window.clearTimeout(callback);   
+                });
+            });
+        }
+        element.appendChild(image);
+    } else {
+        const image = createImage("/static/nopic.webp" , "no image", 'card-img-top w-100');
         element.appendChild(image);
     }
     var wrapper = null;
