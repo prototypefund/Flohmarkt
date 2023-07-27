f1eec28e3c5eb68d038b7f5a3e974cb45ae03c23
grindhold <grindhold@gmx.net>
2023-07-27 20:32:57 +0200

    put instance setting retrieval in correct place

--- flohmarkt/__init__.py
+++ flohmarkt/__init__.py
@@ -83,13 +83,13 @@ async def root(request: Request):
 async def other(request: Request, user: str, item: str):
     if "application/activity+json" in request.headers["accept"]:
         headers = {"Content-type":"application/activity+json"}
-        settings = await InstanceSettingsSchema.retrieve()
         item = await ItemSchema.retrieve_single_id(item)
         user = await UserSchema.retrieve_single_name(user)
         item = await item_to_note(item, user)
         item = await append_context(item)
         return JSONResponse(content=item, headers=headers)
     else:
+        settings = await InstanceSettingsSchema.retrieve()
         item = await ItemSchema.retrieve_single_id(item)
         return templates.TemplateResponse("item.html", {
             "request": request,
