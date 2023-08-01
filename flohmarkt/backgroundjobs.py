04b6334dd180751cb817756203e85320999a6ed3
grindhold <grindhold@gmx.net>
2023-08-01 17:52:20 +0200

    avoid deleting used images

--- flohmarkt/backgroundjobs.py
+++ flohmarkt/backgroundjobs.py
@@ -11,13 +11,31 @@ async def clean_images():
 
     fs_images = os.listdir(os.path.join(cfg["General"]["DataPath"], "images")) 
 
-    item_images = await Database.find({
-        "type": "item",
-    }, fields=["images"])
+    item_images = []
+    item_image_batch = None
+    limit = 100
+    skip = 0
 
-    avatar_images = await Database.find({
-        "type": "user",
-    }, fields=["avatar"])
+    while item_image_batch != []:
+        item_image_batch = await Database.find({
+            "type": "item",
+        }, fields=["images"], limit=limit, skip=skip)
+        limit += 100
+        skip += 100
+        item_images.extend(item_image_batch)
+
+    avatar_images = []
+    avatar_image_batch = None
+    limit = 100
+    skip = 0
+
+    while avatar_image_batch != []:
+        avatar_images = await Database.find({
+            "type": "user",
+        }, fields=["avatar"], limit=limit, skip=skip)
+        limit += 100
+        skip += 100
+        avatar_images.extend(item_image_batch)
 
     item_images = [i["images"] for i in item_images]
     avatar_images = [a["avatar"] for a in avatar_images]
