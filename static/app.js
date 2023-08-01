562c642334b2703b070dcfd53926b9c052ab58b1
grindhold <grindhold@gmx.net>
2023-08-01 17:52:47 +0200

    whitespace fix

--- static/app.js
+++ static/app.js
@@ -107,10 +107,10 @@ export function updateAvatar(avatar) {
     const token = window.sessionStorage.getItem('token');
     if (token != undefined && token != "null" && typeof(token) === 'string') {
         const parsedToken = JSON.parse(window.sessionStorage.getItem('parsedToken'));
-	parsedToken.avatar = avatar;
-	const new_token = JSON.stringify(parsedToken);
-	window.sessionStorage.setItem('parsedToken', new_token);
-	headerAvatar();
+        parsedToken.avatar = avatar;
+        const new_token = JSON.stringify(parsedToken);
+        window.sessionStorage.setItem('parsedToken', new_token);
+        headerAvatar();
     }
 }
 
