5ae784b8cc0ee0bd46c3ce9b50e6306a058b68dd
grindhold <grindhold@gmx.net>
2023-08-03 14:25:01 +0200

    use image uploader in user profile

--- static/user.js
+++ static/user.js
@@ -5,9 +5,7 @@ import { createItem } from "./create/item.js";
 import { createElement } from "./create/element.js";
 import { updateAvatar } from "./app.js";
 import { getCurrentUser } from "./current_user.js"
-
-const MAX_WIDTH = 128,
-      MAX_HEIGHT = 128;
+import "./create/image_uploader.js";
 
 const [items, user, currentUser] = await Promise.all([
     fetchJSON('item/by_user/' + window.USER_ID),
@@ -19,6 +17,8 @@ var watching = [];
 if (currentUser != null && 'watching' in currentUser) {
     watching = currentUser["watching"];
 }
+
+const imageUploader = document.getElementById('image_uploader');
 
 const itemsFragment = document.createDocumentFragment();
 items.forEach(async item => {
@@ -53,10 +53,18 @@ createBtn.addEventListener('click', event => {
     event.preventDefault();
 
     const formData = new FormData(createForm);
-    putJSON("/api/v1/user/"+user.id, {
-        bio: formData.get('bio'),
-        avatar: formData.get('avatar'),
-    })
+
+    const sendData = {
+        bio: formData.get('bio')
+    };
+
+    const imagedata = imageUploader.getData();
+    console.log(imagedata);
+    if (imagedata.length == 1) {
+        sendData.avatar = imagedata[0].image_id;
+    }
+
+    putJSON("/api/v1/user/"+user.id, sendData)
     .then(async data => {
         const new_user = await fetchJSON('user/' + user.id);
 	updateAvatar(new_user.avatar);
@@ -95,107 +103,4 @@ createForm.querySelectorAll('input, textarea').forEach((input, index) => {
     input.addEventListener('change', function() {
         createBtn.disabled = false; // 1111
     });
-});
-
-const uploadInput = document.getElementById('upload');
-uploadInput.addEventListener('change', () => {
-    const gridImages = document.querySelector('.grid__images');
-    for (const file of uploadInput.files) {
-        const fileReader = new FileReader();
-        fileReader.onload = event => {
-            let srcData = event.target.result;
-            const image = createImg(srcData, file.name, 'd-block');
-            image.onload = () => {
-                let width = image.width,
-                    height = image.height,
-                    resize;
-            
-                if (width > height) {
-                    if (width > MAX_WIDTH) {
-                        height *= MAX_WIDTH / width;
-                        width = MAX_WIDTH;
-                        resize = true;
-                    }
-                }
-                if (height > MAX_HEIGHT) {
-                    width *= MAX_HEIGHT / height
-                    height = MAX_HEIGHT
-                    resize = true
-                }
-                if (resize) {
-                    const canvas = document.createElement('canvas'),
-                    context = canvas.getContext('2d')
-                    
-                    canvas.width = width
-                    canvas.height = height
-                    
-                    context.drawImage(image, 0, 0, width, height)
-                    srcData = canvas.toDataURL('image/*')
-                }
-                window.fetch('/api/v1/image/', {
-                        method: "post",
-                        headers: {
-                            "Authorization": "Bearer "+window.sessionStorage.getItem("token")
-                        },
-                        body: srcData
-                    }
-                ).then(data=>data.json()
-                ).then(id=>{
-                    const avatar_input = document.getElementById('avatar');
-                    avatar_input.value = id;
-                });
-            }
-            function isBefore(el1, el2) {
-                let cur;
-                if (el2.parentNode === el1.parentNode) {
-                  for (cur = el1.previousSibling; cur; cur = cur.previousSibling) {
-                    if (cur === el2) return true;
-                  }
-                }
-
-                return false;
-            }
-            const li = createElement('li');
-            li.classList.add('position-relative');
-            li.classList.add('p-0');
-            li.draggable = false;
-            li.addEventListener('dragend', () => {
-                selected = null;
-                li.draggable = false;
-            });
-            li.addEventListener('dragover', event => {
-                const parent = event.target.parentNode;
-                if (parent.tagName !== 'LI') return;
-                if (event.target.closest('li').isEqualNode(selected)) return;
-                if (isBefore(selected, parent)) {
-                    gridImages.insertBefore(selected, parent);
-                } else {
-                    gridImages.insertBefore(selected, parent.nextSibling)
-                }
-            });
-            li.addEventListener('dragstart', event => {
-                event.dataTransfer.effectAllowed = 'move';
-                event.dataTransfer.setData('text/plain', null); // still needed for Firefox?
-                selected = event.target;
-            });
-            li.appendChild(image);
-            const iconDelete = createSVG('x');
-            iconDelete.addEventListener('click', () => {
-                li.remove();
-            });
-            li.appendChild(iconDelete);
-            const iconGrab = createSVG('hand-grab');
-            iconGrab.addEventListener('pointerdown', () => {
-                li.draggable = true;
-            });
-            iconGrab.addEventListener('pointerup', () => {
-                li.draggable = false;
-            });
-            li.appendChild(iconGrab);
-            window.requestAnimationFrame(() => {
-                gridImages.appendChild(li);
-            });
-        }
-        fileReader.readAsDataURL(file);
-    };
 });
