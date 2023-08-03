d5bceca96b7a79791fa9fed45e16fa6a03d22abe
grindhold <grindhold@gmx.net>
2023-08-03 20:45:56 +0200

    also react to application/json accept on item-route

--- flohmarkt/__init__.py
+++ flohmarkt/__init__.py
@@ -96,7 +96,8 @@ async def root(request: Request):
 @app.get("/~{user}/{item}")
 async def other(request: Request, user: str, item: str):
     if "application/activity+json" in request.headers["accept"] or \
-       "application/ld+json" in request.headers["accept"]: 
+       "application/ld+json" in request.headers["accept"] or \
+       "application/json" in request.headers["accept"]: 
         headers = {"Content-type":"application/activity+json"}
         item = await ItemSchema.retrieve_single_id(item)
         user = await UserSchema.retrieve_single_name(user)
