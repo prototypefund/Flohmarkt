05b661ae12e55056f3629040a2c6fd64fc4c9447
grindhold <grindhold@gmx.net>
2023-08-11 21:36:07 +0200

    fix user dialog

--- static/user.js
+++ static/user.js
@@ -1,11 +1,8 @@
-import { fetchJSON, putJSON, deleteCall } from "./utils.js";
+import { fetchJSON } from "./utils.js";
 import { createImg } from "./create/img.js";
-import { createSVG } from "./create/svg.js";
 import { createItem } from "./create/item.js";
 import { createElement } from "./create/element.js";
-import { updateAvatar } from "./app.js";
 import { getCurrentUser } from "./current_user.js"
-import "./create/image_uploader.js";
 
 const [items, user, currentUser] = await Promise.all([
     fetchJSON('item/by_user/' + window.USER_ID),
@@ -18,8 +15,6 @@ if (currentUser != null && 'watching' in currentUser) {
     watching = currentUser["watching"];
 }
 
-const imageUploader = document.getElementById('image_uploader');
-
 const itemsFragment = document.createDocumentFragment();
 items.forEach(async item => {
     itemsFragment.appendChild(createItem(item, false, watching));
@@ -27,7 +22,6 @@ items.forEach(async item => {
 
 const userFragment = document.createDocumentFragment();
 
-const token = JSON.parse(window.sessionStorage.getItem('parsedToken'));
 var watched_items = [];
 const watchedFragment = document.createDocumentFragment();
 
@@ -39,59 +33,3 @@ window.requestAnimationFrame(() => {
     colAbout.appendChild(userFragment);
 });
 
-const createBtn = document.getElementById('create-btn');
-createBtn.addEventListener('click', event => {
-    event.preventDefault();
-
-    const formData = new FormData(createForm);
-
-    const sendData = {
-        bio: formData.get('bio')
-    };
-
-    const imagedata = imageUploader.getData();
-    console.log(imagedata);
-    if (imagedata.length == 1) {
-        sendData.avatar = imagedata[0].image_id;
-    }
-
-    putJSON("/api/v1/user/"+user.id, sendData)
-    .then(async data => {
-        const new_user = await fetchJSON('user/' + user.id);
-	updateAvatar(new_user.avatar);
-	window.location.pathname = '/~' + new_user.name;
-    });
-});
-
-const deleteButton = document.getElementById('delete-btn');
-deleteButton.addEventListener('click', async e => {
-    if (prompt("Please enter your username to confirm account deletion!") == user.name) {
-        const res = await deleteCall('/api/v1/user/'+user.id);
-        if (res == true) {
-            window.sessionStorage.setItem('token',null);
-            window.sessionStorage.setItem('parsedToken',null);
-            window.location = "/";
-        }
-    } else {
-        alert("Wrong usename. Aborting account deletion");
-    }
-});
-
-const notificationButton = document.getElementById('notification-btn');
-notificationButton.addEventListener('click', ()=>{
-    if (!("Notification" in window)) {
-        return;
-    }
-    if (Notification.permission !== "granted") {
-        Notification.requestPermission();
-    }
-});
-
-
-const createForm = document.getElementById('create-form');
-let inputValid = 0;
-createForm.querySelectorAll('input, textarea').forEach((input, index) => {
-    input.addEventListener('change', function() {
-        createBtn.disabled = false; // 1111
-    });
-});
--- static/usersettings.js
+++ static/usersettings.js
@@ -1,12 +1,42 @@
-import { fetchJSON, postJSON, deleteCall } from "./utils.js";
+import { fetchJSON, putJSON, deleteCall } from "./utils.js";
 import { initTabs } from "./tabs.js";
+import { getCurrentUser } from "./current_user.js";
+import { updateAvatar } from "./app.js";
 import "./create/image_uploader.js";
 
+const imageUploader = document.getElementById('image_uploader');
 initTabs();
 
 const [currentUser] = await Promise.all([
     getCurrentUser
 ]);
+
+const saveForm = document.getElementById('save-form');
+const saveBtn = document.getElementById('save-btn');
+saveBtn.addEventListener('click', event => {
+    event.preventDefault();
+
+    const formData = new FormData(saveForm);
+
+    const sendData = {
+        bio: formData.get('bio')
+    };
+
+    const imagedata = imageUploader.getData();
+    console.log(imagedata);
+    if (imagedata.length == 1) {
+        sendData.avatar = imagedata[0].image_id;
+    }
+
+    putJSON("/api/v1/user/"+currentUser.id, sendData)
+    .then(async data => {
+        const new_user = await fetchJSON('user/' + currentUser.id);
+	updateAvatar(new_user.avatar);
+	window.location.pathname = '/~' + new_user.name;
+    });
+});
+const bio_entry = document.getElementById('bio');
+bio_entry.value = currentUser.bio;
 
 const deleteButton = document.getElementById('delete-btn');
 deleteButton.addEventListener('click', async e => {
